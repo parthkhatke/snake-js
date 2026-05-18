907ffc2fa80003bf4097f0fd6e26685eeeb2107c
parthkhatke <parthkhatke@gmail.com>
2026-05-18 23:13:06 +0530

    added snake object and function

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 const board = document.querySelector(".board");
-const blockWidth = 50 ;
-const blockHeight = 50 ; 
+const blockWidth = 50;
+const blockHeight = 50;
 
-//array 
-const blocks=[];
-//get number of row and col 
-const cols = Math.floor(board.clientWidth/blockWidth);
-const rows = Math.floor(board.clientHeight/blockHeight);
+//array
+const blocks = [];
+//get number of row and col
+const cols = Math.floor(board.clientWidth / blockWidth);
+const rows = Math.floor(board.clientHeight / blockHeight);
 
-//create board 
+const snake = [
+  {
+    x: 1,
+    y: 3,
+  },
+  {
+    x: 1,
+    y: 4,
+  },
+  {
+    x: 1,
+    y: 5,
+  },
+];
+//create board
 
-for(let row=0;row<rows;row++)
+for (let row = 0; row < rows; row++) {
+  for (let col = 0; col < cols; col++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    board.appendChild(block);
+    blocks[`${row}-${col}`] = block;
+  }
+}
+
+
+function renderSnake()
 {
-    for(let col=0;col<cols;col++)
+    snake.forEach( body => 
     {
-      const block= document.createElement('div') ;
-      block.classList.add("block") ;
-      board.appendChild(block) ;
-      blocks[`${row}-${col}`] = block
+        console.log(blocks[`${body.x}-${body.y}`]);
+        
     }
+    )
 }
