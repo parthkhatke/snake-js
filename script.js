9d80ffd4b4b48d08adf374973837b35faf9c5fa6
parthkhatke <parthkhatke@gmail.com>
2026-05-18 23:45:27 +0530

    added funciton to render snake every 300 ms

--- script.js
+++ script.js
@@ -7,20 +7,14 @@ const blocks = [];
 //get number of row and col
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
-
+//snake direction
+let direction="down";
 const snake = [
   {
     x: 1,
     y: 3,
   },
-  {
-    x: 1,
-    y: 4,
-  },
-  {
-    x: 1,
-    y: 5,
-  },
+
 ];
 //create board
 
@@ -30,6 +24,7 @@ for (let row = 0; row < rows; row++) {
     block.classList.add("block");
     board.appendChild(block);
     blocks[`${row}-${col}`] = block;
+    block.innerHTML=`${row}-${col}`
   }
 }
 
@@ -38,8 +33,35 @@ function renderSnake()
 {
     snake.forEach( body => 
     {
-        console.log(blocks[`${body.x}-${body.y}`]);
-        
+       blocks[`${body.x}-${body.y}`].classList.add("fill");
+       
     }
     )
 }
+
+
+//rendersnake every 3ms 
+setInterval(
+  ()=>
+  {
+    let head = null;
+    // if(direction==="left")
+    // {
+    //   head = {x:snake[0].x, y:snake[0].y-1};
+    // }
+    direction==="left" ? head = {x:snake[0].x, y:snake[0].y-1} : 
+    direction==="right"? head = {x:snake[0].x, y:snake[0].y+1} :
+    direction==="up"   ? head = {x:snake[0].x-1, y:snake[0].y} :
+                         head = {x:snake[0].x+1, y:snake[0].y} ;
+    
+    snake.forEach( body => 
+    {
+       blocks[`${body.x}-${body.y}`].classList.remove("fill");
+        
+    })
+    snake.unshift(head) ;
+    snake.pop();
+    
+
+    renderSnake();
+  },400)
